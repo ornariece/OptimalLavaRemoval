a0b822845185006badcd18c84fe0239b4c4498e2
François <franvoisoder@gmail.com>
2018-04-24 22:04:42 +0200

    ajout de binary

--- naive_algorithme.py
+++ naive_algorithme.py
@@ -1,4 +1,17 @@
 # Algo naïf pour résoudre le pb
 
 
+def binary(n, length):
+    res = []
+    while n != 0:
+        a = n%2
+        if a == 1:
+           res = [1] + res
+        elif a == 0:
+            res = [0] + res
+        n = n//2
+    while len(res) < length:
+        res = [0] + res
+    return res
+
 
