3edb7067e1ece4f3bdbfbc28a5b98d5bef2209e7
François <franvoisoder@gmail.com>
2018-04-24 22:15:55 +0200

    ajout de is_correct et cover

--- naive_algorithme.py
+++ naive_algorithme.py
@@ -1,5 +1,8 @@
 # Algo naïf pour résoudre le pb
 
+import numpy
+from copy import deepcopy
+
 
 def binary(n, length):
     res = []
@@ -14,4 +17,18 @@ def binary(n, length):
         res = [0] + res
     return res
 
+
+def is_correct(Map):
+    return not 1 in Map
+
+
+def cover(Map, Centers):
+    mod = []
+    for center in Centers:
+        for i in range(15):
+            for j in range(15):
+                mod += [center + (i,j), center + (-i,j), center + (i, -j), center + (-i, -j)]
+    for coord in mod:
+        Map[coord] = 0
+    return Map
 
