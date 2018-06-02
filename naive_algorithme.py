e636cd6a6b0eae0681dd32444aea4cc4b693d6a2
François <franvoisoder@gmail.com>
2018-06-02 17:07:07 +0200

    pep8

--- naive_algorithme.py
+++ naive_algorithme.py
@@ -18,41 +18,40 @@ def binary(n, length):
             res = [1] + res
         elif a == 0:
             res = [0] + res
-        n = n//2
+        n //= 2
     while len(res) < length:
         res = [0] + res
     return res
 
 
-def ind2lc(ind, nbl, nbc):
+def ind2lc(ind, nbc):
     """
 
     :param ind: the indice of a position
-    :param nbl: the number of lines in the map
     :param nbc: the number of columns in the map
     :return: (number_of_lines, number_of_columns) of this position
     """
     return ind//nbc, ind % nbc
 
 
-def is_correct(Map):
+def is_correct(cv):
     """
 
-    :param Map: a array of positions, 0 for bloc and 1 for lava
-    :return: the bool saying if there isn't anymore lava in the map
+    :param cv: a array of positions, 0 for bloc and 1 for lava
+    :return: the bool saying if there isn't anymore lava in the map (the cover is correct)
     """
-    return 1 not in Map
+    return 1 not in cv
 
 
-def cover(M, Centers):
+def cover(mp, centers):
     """
 
-    :param M: an array of positions (a map)
-    :param Centers: a list of coordinates witch indicates where to place the centers of squares to cover the map
+    :param mp: an array of positions (a map)
+    :param centers: a list of coordinates witch indicates where to place the centers of squares to cover the map
     :return: the map after cover
     """
-    nbl, nbc = M.shape
-    for center in Centers:
+    nbl, nbc = mp.shape
+    for center in centers:
         x = center[0]
         y = center[1]
         l_min = max(0, x - 14)
@@ -62,39 +61,39 @@ def cover(M, Centers):
         l = l_max - l_min
         c = c_max - c_min
         rng = np.zeros((l, c))
-        M[l_min:l_max, c_min:c_max] = rng
-    return M
+        mp[l_min:l_max, c_min:c_max] = rng
+    return mp
 
 
-def good_cover(Map):
+def good_cover(map1):
     """
-    :type Map: np.ndarray
-    :param Map: an array of positions (a map)
+    :type map1: np.ndarray
+    :param map1: an array of positions (a map)
     :return: the list of list of centers that cover the map so that there isn't anymore lava
     """
-    Res = []
-    (nbl, nbc) = Map.shape
+    res = []
+    (nbl, nbc) = map1.shape
     for i in range(2**(nbl*nbc)):
-        Centers = []
-        M = deepcopy(Map)
-        D = binary(i, nbl*nbc)
-        for j in range(len(D)):
-            if D[j] == 1:
-                Centers += [ind2lc(j, nbl, nbc)]
-        M = cover(M, Centers)
-        if is_correct(M):
-            Res += [(i, Centers)]
-    return Res
+        centers = []
+        mp = deepcopy(map1)
+        d = binary(i, nbl*nbc)
+        for j in range(len(d)):
+            if d[j] == 1:
+                centers += [ind2lc(j, nbc)]
+        mp = cover(mp, centers)
+        if is_correct(mp):
+            res += [(i, centers)]
+    return res
 
 
-def best_cover(Map):
+def best_cover(map_):
     """
 
-    :param Map: an array of positions (a map)
+    :param map_: an array of positions (a map)
     :return: the best cover for the map: the one that use the less squares
     """
-    Covers = good_cover(Map)
-    return min(Covers, key=lambda x: len(x[1]))
+    covers = good_cover(map_)
+    return min(covers, key=lambda x: len(x[1]))
 
 
 print(best_cover(np.array([[0, 1], [0, 1]])))
